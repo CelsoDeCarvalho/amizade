7ecc511b6826a2efddd8791c33c417b7ed7f68ae
celso <celsomomade361@gmail.com>
2021-08-11 08:29:43 +0200

    refactorando...

--- index.php
+++ index.php
@@ -5,26 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="./bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="./bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./view/css/index.css">
-    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <title>Par perfeito</title>
+    <title>Amizade</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Par<i class='bx bx-male'></i><span><i class='bx bx-female' ></i>Perfeito</span> </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-            </ul>
-                <button class="btn btn-logar">Entrar</button>
-        </div>
-    </div>
-</nav>
 </body>
 </html>
